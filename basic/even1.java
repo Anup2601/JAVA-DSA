fb3f42f670dce7c210ff55966a802a3c564d9009
Anup Mishra <anupm0873@gmail.com>
2024-09-24 09:14:36 +0530

    even nnumber by function

---
+++ basic/even1.java
@@ -0,0 +1,13 @@
+public class even1{
+    public static boolean  iseven(int num){
+        if(num%2==0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args) {
+    System.out.println(iseven(10)); 
+    }
+}
