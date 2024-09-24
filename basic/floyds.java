f3dd435bc6533cca82515790cfe9a1f38596155e
Anup Mishra <anupm0873@gmail.com>
2024-09-24 10:08:19 +0530

    0-1 Triangle

--- basic/floyds.java
+++ basic/floyds.java
@@ -1,7 +1,9 @@
 class floyds{
     public static void main(String[] args) {
         int count=1;
+        // outer loop
         for(int i=1;i<=5;i++){
+            // inner loop 
             for(int j=1;j<=i;j++){
                 System.out.print("  "+count);
                 count++;
