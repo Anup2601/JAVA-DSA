7c39f1a89086188364b98a34966505ee3418b61a
Anup Mishra <anupm0873@gmail.com>
2024-11-12 10:18:17 +0530

    no. of 7 in array

---
+++ 2darray/q1.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+class q1{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[][] arr=new int[3][3];
+        System.out.println("enter your arr");
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                arr[i][j]=sc.nextInt();
+            }
+        }
+        int count=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]==7){
+                    count++;
+                }
+            }
+        }
+        System.out.println("no of 7's is :-"+count);
+        sc.close();
+    }
+}
