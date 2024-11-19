95b021c556a8510ebed5bcd26855e831cc38973d
Anup Mishra <anupm0873@gmail.com>
2024-11-19 22:29:02 +0530

    String Compression

--- string/convertToUppercase.java
+++ string/convertToUppercase.java
@@ -1,20 +1,3 @@
-// class convertToUppercase{
-//     public static void main(String[] args) {
-//         StringBuilder sb=new StringBuilder();
-//         String s="hi my name is anup";
-//         char c=Character.toUpperCase(s.charAt(0));
-//         sb.append(c);
-//         for (int i = 1; i < s.length(); i++) {
-//             if(s.charAt(i)==' '&& i<s.length()-1){
-//                 sb.append(s.charAt(i));
-//                 i++;
-//                 sb.append(Character.toUpperCase(s.charAt(i)));
-//             }
-//             else sb.append(s.charAt(i));
-//         }
-//         System.out.println(sb);
-//     }
-// }
 class convertToUppercase{
     public static void main(String[] args) {
         String s="hi my name is anup";
