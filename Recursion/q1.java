eb3c1906677f4da92e2aeb81864bb423fed37c4b
Anup Mishra <anupm0873@gmail.com>
2025-01-11 22:55:30 +0530

    basic of recursion

---
+++ Recursion/q1.java
@@ -0,0 +1,14 @@
+class q1{
+    public  static void printDec(int num){
+        if(num==1){
+            System.out.println(num);
+            return;
+        }
+        System.out.println(num+" ");
+        printDec(num-1);
+    }
+    public static void main(String[] args) {
+         int n=10;
+         printDec(n);
+    }
+}
