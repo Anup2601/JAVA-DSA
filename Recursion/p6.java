19fbf96a5d26549ef9195384a37db38deb4c520c
Anup Mishra <anupm0873@gmail.com>
2025-01-27 13:41:21 +0530

    first occurance of array

--- Recursion/p6.java
+++ Recursion/p6.java
@@ -1,16 +1,18 @@
+// sort the array
 public class p6 {
     public static boolean sort(int arr[],int i){
         if(arr[i]==arr.length-1){
             return true;
         }
-        if(arr[i]<=arr[i+1]){
-        sort(arr,i+1);
-        return true;
-    }
-    return true;
+        if(arr[i]>arr[i+1]){
+            return false;
+        }
+    return sort(arr,i+1);
     }
     public static void main(String[] args) {
-        int[] arr=[1,2,3,4,5,6,7];
-        int t=sort(arr, i)
+        int[] arr={1,2,3,4,5,6,7};
+        boolean t=sort(arr, 0);
+        String r=t==true?"sort":"not-sort";
+        System.out.println(r);
     }
 }
---
+++ Recursion/p7.java
@@ -0,0 +1,16 @@
+public class p7 {
+    public static int FO(int[] arr,int i,int key ){
+        if(arr[i]==arr.length-1){
+            return 0;
+        }
+        if(arr[i]==key){
+            return i;
+        }
+        return FO(arr, i+1, key);
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5,5,4,2,3,43,2,4,4,33,4,3};
+        int k=33;
+        System.out.println(FO(arr, 0, k));
+    }
+}
