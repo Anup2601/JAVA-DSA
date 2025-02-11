d703911b0d829db2beb7bb5be660d3ad20ac5b94
Anup Mishra <anupm0873@gmail.com>
2025-02-11 20:59:36 +0530

    all occurance of element

---
+++ Recursion/pq1.java
@@ -0,0 +1,16 @@
+class pq1{
+    public static void occurance(int [] arr, int key, int i){
+        if(i==arr.length){
+            return;
+        }
+        if(arr[i]==key){
+            System.out.println(i+" ");
+        }
+        occurance(arr, key, i+1);
+    }
+    public static void main(String[] args) {
+        int [] arr={3, 2, 4, 5, 6, 2, 7, 2, 2};
+        int key=2;
+        occurance(arr, key, 0);
+    }
+}
