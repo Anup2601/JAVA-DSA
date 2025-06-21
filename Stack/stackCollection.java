5dd000c10c8f510ddd9497935290ed7ceefabd13
Anup Mishra <anupm0873@gmail.com>
2025-06-21 16:42:47 +0530

    stack using collection framwork

---
+++ Stack/stackCollection.java
@@ -0,0 +1,16 @@
+
+import java.util.Stack;
+
+public class stackCollection {
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
