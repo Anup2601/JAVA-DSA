94adb4e673d9db2ca92b98d6b3478622d0263458
Anup Mishra <anupm0873@gmail.com>
2025-07-07 23:52:14 +0530

    activaty selection

---
+++ Greedy/ActivitySelection.java
@@ -0,0 +1,41 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+
+public class ActivitySelection {
+    public static void main(String[] args) {
+        int[] start={1,3,0,5,8,5};
+        int[] end={2,4,6,7,9,9};
+        // shorting 
+        int Activities[][]= new int[start.length][3];
+        for(int i=0;i<start.length;i++){
+            Activities[i][0]=i;
+            Activities[i][1]=start[i];
+            Activities[i][2]=end[i];
+        }
+
+        // lamda function
+        Arrays.sort(Activities, Comparator.comparingDouble(o->o[2]));
+        // End Time basis sort
+        int maxActivity=0;
+        ArrayList<Integer> ans =new ArrayList<>();
+        // 1st activity
+        maxActivity=1;
+        ans.add(Activities[0][0]);
+        int lastEnd=Activities[0][2];
+        for(int i=0;i<end.length;i++){
+            if(start[i]>=lastEnd){
+                maxActivity++;
+                ans.add(Activities[i][0]);
+                lastEnd=Activities[i][2];
+            }
+        }
+        System.out.println("MaxActivity is :- "+ maxActivity);
+        for(int i=0;i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+ " ");
+        }
+        System.out.println();
+    }
+}
