69846d9dbe8197893c739fe3b81c3b5c0da166db
Anup Mishra <anupm0873@gmail.com>
2025-07-21 12:51:35 +0530

    level of order of tree

--- BinaryTree/preOrder.java
+++ BinaryTree/preOrder.java
@@ -21,11 +21,21 @@ class preOrder{
             newNode.right=BuildTree(nodes);
             return newNode;
         }
+        public static void preorder(Node root){
+            if(root==null){
+                System.out.println("-1");
+                return;
+            }
+            System.out.println(root.data+ " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
     }
     public static void main(String[] args) {
         int[] nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree =new BinaryTree();
         Node root=tree.BuildTree(nodes);
         System.out.println(root.data);
+        tree.preorder(root);
     }
 }
